8416d55a0a1cb0da7b2a9621687fcae6ac443da3
Rafael Faria <joaorafaelfaria@gmail.com>
2018-11-09 20:45:18 -0300

    adicionando api do maps

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as VueGoogleMaps from "vue2-google-maps"
+import router from './router'
+import Vuetify from '../node_modules/vuetify'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuetify)
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "REPLACE-THIS-WITH-YOUR-KEY-FROM-ABOVE",
+    libraries: "places" // necessary for places input
+  }
+});
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
+
+
+
