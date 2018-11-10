22ca2e7bbc55fcd2002eaa923dace19723c605a0
Rafael Faria <joaorafaelfaria@gmail.com>
2018-11-10 08:04:13 -0300

    push p alinhar

---
+++ src/components/views/Curriculo.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-container fluid fill-height class="ma-0 pa-0">
+        <v-layout xs12 column>
+            <v-flex xs1 style="background-color:#024FA1; display:flex; flex-direction: row; justify-content:center; align-items:center;">
+                <h1 style="width:100%; text-align:center; color:white; font-size:16px">Fazer o meu currículo</h1>
+            </v-flex>
+            <v-flex xs1></v-flex>
+            
+            
+              
+
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+
+      };
+    } 
+  }
+  
+</script>
+
+
+<style scoped>
+    *{
+    font-family:'Helvetica';
+    }
+
+    
+</style>
