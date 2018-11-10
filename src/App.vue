274347a900656d45c208ef1ec2caa2897d11b851
Thiago Augusto <martinsthiagoaugusto@gmail.com>
2018-11-10 05:42:20 -0300

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app id="inspire">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <v-navigation-drawer
       v-model="drawer"
       fixed
@@ -62,17 +63,46 @@
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title>Chega no Trabalho</v-toolbar-title>
     </v-toolbar>
+
     <v-content>
       <router-view></router-view>
     </v-content>
-    <v-footer color="indigo" app>
-      <span class="white--text">&copy; 2017</span>
+    
+    <v-footer  fixed height="auto"  app>
+      <v-bottom-nav  style="margin: auto;" row wrap :active.sync="bottomNav" :value="true" absolute>
+        <v-btn  to="/vagas" style="height:100%; padding:auto;"  flat value="vagas" >
+          <div class="footerIcon">
+            <i class="fas fa-home"></i>
+          </div>
+        </v-btn>
+
+        <v-btn  to="/mapas" style=""  flat value="mapas" >
+          <div class="footerIcon">
+            <i class="fas fa-book-open"></i>  
+          </div>
+        </v-btn>
+
+        <v-btn  to="/perfil" style=""  flat value="perfil" >
+          <div class="footerIcon">
+            <i class="fas fa-cog"></i>
+          </div>
+        </v-btn>
+
+        <v-btn  to="/filtro" style="" flat value="filtro" >
+          <div class="footerIcon">
+            <i class="fas fa-filter"></i>
+          </div>
+        </v-btn>
+
+      </v-bottom-nav>
     </v-footer>
+
   </v-app>
 </template>
 
 <script>
 /* eslint-disable */
+import router from './router/index.js';
 export default {
   name: 'App',
   // components: {
@@ -81,12 +111,55 @@ export default {
   data () {
     return {
       drawer: null,
+      showingNav: true,
+      bottomNav: ''
     }
   },
   methods: {
     teste () {
 
     }
+  },
+  created: function(){ 
+    if(router.currentRoute.name == 'Vagas'){
+      this.bottomNav = 'vagas'
+    }
+    else if(router.currentRoute.name == 'Mapas'){
+       this.bottomNav = 'mapas' 
+    }
+    else if(router.currentRoute.name == 'Perfil'){
+        this.bottomNav = 'perfil' 
+    }
+    else if(router.currentRoute.name == 'Filtro'){
+        this.bottomNav = 'filtro' 
+    }
+    
+  },
+
+  computed:{
+    
+
+      
   }
 }
 </script>
+
+<style scoped>
+  .footerBar{
+    display:none;
+  }
+  .footerBar2{
+    display:flex;
+  }
+  .footerIcon{
+    display:flex;
+    height:100%;
+    flex: 1 1 20%;
+    justify-content:center;
+    align-items:center;
+    font-size:28px;
+    text-decoration:none;
+    color:#024FA1;
+  }
+
+</style>
