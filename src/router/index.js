2f0c177a5eb7157afb2ce29350f501cdd9e75b14
Thiago Augusto <martinsthiagoaugusto@gmail.com>
2018-11-09 21:14:46 -0300

    Adicionado escopo geral

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/views/HelloWorld'
+import Vagas from '@/components/views/Vagas'
 Vue.use(Router)
 export default new Router({
   routes: [
@@ -8,6 +9,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/vagas',
+      name: 'Vagas',
+      component: Vagas
     }
   ]
 })
