8a7e8fa2ada81c869c4bb0b3a4af95c19fa1f6e1
Thiago Augusto <martinsthiagoaugusto@gmail.com>
2018-12-17 10:21:33 -0300

    Adicionado versoes para build

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
@@ -10,12 +11,47 @@ Vue.use(VueJsonp)
 
 Vue.config.productionTip = false
 
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 import firebaseConfig from '../firebaseConfig.json';
 
 export const firebaseApp = firebase.initializeApp(firebaseConfig)
 export const db = firebase.firestore();
 db.settings({timestampsInSnapshots: true});
+
+// const messaging = firebaseApp.messaging();
+// messaging.usePublicVapidKey("BEXEaHALjNHXUKhplPwHL7EYYIKVC0Wu_FLKUeZmq2n_HlhNyutWGggr0VPg0qbjyMRY-oB6usVYyVmX6e_bp_I");
+// messaging.requestPermission().then(function() {
+//   console.log('Notification permission granted.');
+//   // TODO(developer): Retrieve an Instance ID token for use with FCM.
+//   // ...
+// }).catch(function(err) {
+//   console.log('Unable to get permission to notify.', err);
+// });
+
+// messaging.getToken().then(function(currentToken) {
+//   if (currentToken) {
+//     console.log(currentToken)
+//     // sendTokenToServer(currentToken);
+//     // updateUIForPushEnabled(currentToken);
+//   } else {
+//     // Show permission request.
+//     console.log('No Instance ID token available. Request permission to generate one.');
+//     // Show permission UI.
+//     // updateUIForPushPermissionRequired();
+//     // setTokenSentToServer(false);
+//   }
+// }).catch(function(err) {
+//   console.log('An error occurred while retrieving token. ', err);
+//   // showToken('Error retrieving Instance ID token. ', err);
+//   // setTokenSentToServer(false);
+// });
+
+// messaging.onMessage(payload => {
+//   console.log(payload)
+// })
+
+
 
 Vue.use(VueGoogleMaps, {
   load: {
